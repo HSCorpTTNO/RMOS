5539a74b978a358f717214e1c9e6c9b9a7116773
Ian-Moffett <hscorp99906@gmail.com>
2022-03-15 12:42:50 -0400

    Re-added fadt because I am stupid.

--- kernel/src/drivers/acpi/tables.h
+++ kernel/src/drivers/acpi/tables.h
@@ -25,7 +25,7 @@ typedef struct {
 } __attribute__((packed)) acpi_xsdp;
 
 
-typedef struct acpi_header_t {
+typedef struct {
     char signature[4];
     uint32_t length;
     uint8_t revision;
@@ -38,15 +38,106 @@ typedef struct acpi_header_t {
 } __attribute__((packed)) acpi_header_t;
 
 
-typedef struct acpi_rsdt_t {
+typedef struct {
     acpi_header_t header;
     uint32_t tables[];
 } __attribute__((packed)) acpi_rsdt_t;
 
 
-typedef struct acpi_xsdt_t {
+typedef struct {
     acpi_header_t header;
     uint64_t tables[];
 } __attribute__((packed)) acpi_xsdt_t;
+
+
+typedef struct {
+    uint8_t address_space;
+    uint8_t bit_width;
+    uint8_t bit_offset;
+    uint8_t access_size;
+    uint64_t base;
+} __attribute__((packed)) acpi_gas_t;
+
+
+typedef struct {
+    acpi_header_t header;
+    uint32_t firmware_control;
+    uint32_t dsdt; // pointer to dsdt
+
+    uint8_t reserved;
+
+    uint8_t profile;
+    uint16_t sci_irq;
+    uint32_t smi_command_port;
+    uint8_t acpi_enable;
+    uint8_t acpi_disable;
+    uint8_t s4bios_req;
+    uint8_t pstate_control;
+    uint32_t pm1a_event_block;
+    uint32_t pm1b_event_block;
+    uint32_t pm1a_control_block;
+    uint32_t pm1b_control_block;
+    uint32_t pm2_control_block;
+    uint32_t pm_timer_block;
+    uint32_t gpe0_block;
+    uint32_t gpe1_block;
+    uint8_t pm1_event_length;
+    uint8_t pm1_control_length;
+    uint8_t pm2_control_length;
+    uint8_t pm_timer_length;
+    uint8_t gpe0_length;
+    uint8_t gpe1_length;
+    uint8_t gpe1_base;
+    uint8_t cstate_control;
+    uint16_t worst_c2_latency;
+    uint16_t worst_c3_latency;
+    uint16_t flush_size;
+    uint16_t flush_stride;
+    uint8_t duty_offset;
+    uint8_t duty_width;
+
+    // cmos registers
+    uint8_t day_alarm;
+    uint8_t month_alarm;
+    uint8_t century;
+
+    // ACPI 2.0 fields
+    uint16_t iapc_boot_flags;
+    uint8_t reserved2;
+    uint32_t flags;
+
+    acpi_gas_t reset_register;
+    uint8_t reset_command;
+    uint16_t arm_boot_flags;
+    uint8_t minor_version;
+
+    uint64_t x_firmware_control;
+    uint64_t x_dsdt;
+
+    acpi_gas_t x_pm1a_event_block;
+    acpi_gas_t x_pm1b_event_block;
+    acpi_gas_t x_pm1a_control_block;
+    acpi_gas_t x_pm1b_control_block;
+    acpi_gas_t x_pm2_control_block;
+    acpi_gas_t x_pm_timer_block;
+    acpi_gas_t x_gpe0_block;
+    acpi_gas_t x_gpe1_block;
+} __attribute__((packed)) acpi_fadt_t;
+
+
+typedef struct {
+    acpi_header_t header;
+    acpi_gas_t ec_control;
+    acpi_gas_t ec_data;
+    uint32_t uid;
+    uint8_t gpe_bit;
+    uint8_t ec_id[];
+} __attribute__((packed)) acpi_ecdt_t;
+
+
+typedef struct {
+    acpi_header_t header;
+    uint8_t data[];
+} __attribute__((packed)) acpi_aml_t;
 
 #endif

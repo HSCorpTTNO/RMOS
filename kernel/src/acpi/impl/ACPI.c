d656536296a90ed3ea230267ad1817fb96b05481
Ian-Moffett <hscorp99906@gmail.com>
2022-03-15 19:04:29 -0400

    Fix line 33: Got actual size of tables, Fix line 23: Whoops, greater then 0 means modern.

--- kernel/src/acpi/impl/ACPI.c
+++ kernel/src/acpi/impl/ACPI.c
@@ -17,21 +17,23 @@ uint8_t checksum_valid(acpi_xsdt_t* table) {
 
 void init_acpi(struct stivale2_struct* ss) {
     struct stivale2_struct_tag_rsdp* rsdp_tag = get_tag(ss, STIVALE2_STRUCT_TAG_RSDP_ID);
-
     acpi_rsdp_t* rsdp = (acpi_rsdp_t*)rsdp_tag->rsdp;
 
-    if (rsdp->rev != 0) {
+    // Check if it is legacy hardware. 
+    if (rsdp->rev == 0) {
         kwrite("\033[41;1;37mLEGACY HARDWARE IS NOT SUPPORTED YET. RASING ISR IN VECTOR 0x01.\n\n");
         __asm__ __volatile__("int $0x1");
     }
 
+    // Grab RSDT.
     acpi_rsdt_t* rsdt = (acpi_rsdt_t*)(uint64_t)rsdp->rsdtaddr;
-
     volatile unsigned short tableidx = 0;
 
-    while (tableidx < 10) {   
+    // Iterate through tables.
+    while (tableidx < rsdt->header.length - sizeof(rsdt->header) / 4) {   
         acpi_madt_t* m = (acpi_madt_t*)(uint64_t)rsdt->tables[tableidx];
-    
+   
+        // Find APIC signature.
         if (strncmp(m->header.signature, "APIC", 4)) {
             madt = m;
             break;

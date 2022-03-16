746df3e10282e9bfb8954864c37997fbf8e531e2
Ian-Moffett <hscorp99906@gmail.com>
2022-03-16 09:12:04 -0400

    Re-implemented APIC.

--- kernel/src/cpu/impl/LAPIC.c
+++ kernel/src/cpu/impl/LAPIC.c
@@ -28,4 +28,24 @@ void cpu_lapic_write_reg(uint32_t reg, uint32_t dword) {
 
 
 uint8_t cpu_lapic_init() { 
+    uint32_t eax, ebx, ecx, edx;
+    cpuid_subleaf(1, 0, &eax, &ebx, &ecx, &edx);
+
+    // TODO: Make this switch to legacy PIC instead of APIC if this condition is true.
+    if (!(edx >> 9) & 0x1) {
+        return 0;
+    }
+
+    // Mask all bits of 8259 PIC.
+    outportb(0x21, 0xFF);
+    outportb(0xA1, 0xFF);
+
+    // LAPIC setup
+    cpu_lapic_write_reg(LAPIC_TASK_PRIORITY_REG, 0);
+    uint32_t msr_low;
+    uint32_t msr_high;
+    cpu_get_msr(IA32_APIC_BASE_MSR, &msr_low, &msr_high);       // Fetch current values of APIC MSR.
+    cpu_set_msr(IA32_APIC_BASE_MSR, msr_low | IA32_APIC_BASE_MSR_ENABLE, msr_high);         // OR the current low values by IA32_APIC_BASE_MSR_ENABLE.
+    outportb(0xF0, 0xFF);
+    return 1;
 }
